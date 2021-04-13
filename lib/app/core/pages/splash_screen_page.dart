867bef1840e0342d7ffcfd2fc9789c1c3b6ea39d
goandreus <andres.chavez.crisostomo@gmail.com>
2021-04-13 00:16:57 -0500

    splash screen page (#3)

---
+++ lib/app/core/pages/splash_screen_page.dart
@@ -0,0 +1,77 @@
+import 'package:clean_architecture/app/core/stores/auth_store.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+splashScreenPage() {
+  Future.delayed(Duration(seconds: 3)).whenComplete(() => {
+        Modular.get<AuthStore>().isLogged
+            ? Modular.to.pushNamedAndRemoveUntil("/home", (_) => false)
+            : Modular.to.pushNamedAndRemoveUntil("/login", (_) => false)
+      });
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage>
+    with TickerProviderStateMixin {
+  AnimationController animation;
+  Animation<double> _fadeInFadeOut;
+
+  @override
+  void initState() {
+    super.initState();
+    splashScreenPage();
+    animationController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          FadeTransition(
+            opacity: _fadeInFadeOut,
+            child: Center(
+              child: Text(
+                "Clean"
+                "\nArchitecture",
+                style: TextStyle(fontSize: 40),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void animationController() {
+    animation = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    );
+    _fadeInFadeOut = Tween<double>(begin: 0.0, end: 1).animate(animation);
+
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animation.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        animation.forward();
+      }
+    });
+    animation.forward();
+  }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
+}
